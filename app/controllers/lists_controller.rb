7934c19bc5bd18d0667dda6cd2a96330e62b7d47
victor della faille <victordellafaille@gmail.com>
2021-08-19 16:47:38 +0200

    finished the backend normally

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,25 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmarks = Bookmark.all
+  end
+
+  def create
+    @list = List.create(list_params)
+    redirect_to lists_path
+  end
+
+  def new
+    @list = List.new
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
